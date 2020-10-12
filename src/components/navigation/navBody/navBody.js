6fd894038384de3b2925f121cd7c2d7035d77621
Jonathan-Odia Osahon Emmanuel <emmyodia@gmail.com>
2020-10-12 03:48:45 +0100

    navBody created

---
+++ src/components/navigation/navBody/navBody.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './navBody.css';
+
+function NavBody(){
+    return(
+        <nav className='navBodyContainer'>
+
+        </nav>
+    )
+}
+
+export default NavBody;
