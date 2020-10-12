fde8fcdaf6411ef9b2ffa2bb2ea67fac4dd71c6b
Jonathan-Odia Osahon Emmanuel <emmyodia@gmail.com>
2020-10-12 04:03:25 +0100

    navItems created

--- src/components/navigation/navBody/navBody.js
+++ src/components/navigation/navBody/navBody.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './navBody.css';
+import NavItems from '../navItems/navItems';
 
 function NavBody(){
     return(
         <nav className='navBodyContainer'>
-
+            <NavItems/>
         </nav>
     )
 }
---
+++ src/components/navigation/navItems/navItems.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './navItems.css';
+
+function NavItems(){
+    return(
+        <div className='navItemsContainer'>
+            <button className='navItemsButton'>Testing</button>
+        </div>
+    )
+}
+
+export default NavItems;
