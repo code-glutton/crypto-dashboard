257d45d7baac2e102a19b506f6ea984eba135c00
Jonathan-Odia Osahon Emmanuel <emmyodia@gmail.com>
2020-10-16 17:36:40 +0100

    news table rearranged to match dom tree

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import './header.css'
+import './header.css';
+import {Link} from 'react-router-dom';
 
 function Header(){
     return(
         <header className='headerContainer'>
-            <div className='headerTitleDiv'>
-                <p className='headerTitleText'>Logo</p>
-            </div>
+            <Link to='/'>
+                <div className='headerTitleDiv'>
+                    <p className='headerTitleText'>Logo</p>
+                </div>
+            </Link>
         </header>
     )
 }
--- src/components/news/news.js
+++ src/components/news/news.js
@@ -5,70 +5,73 @@ function News(){
     return(
         <div className='newsContainer'>
             <div className='newsTableContainer'>
-            <table className="customers">
-                <tr>
-                    <th colSpan='2'>
-                        Breaking Crypto News
-                    </th>
-                </tr>
-                <tr>
+                <table className="customers">
+                    <thead>
+                        <tr>
+                            <th colSpan='2'>
+                                Breaking Crypto News
+                            </th>
+                        </tr> 
+                    </thead>
+                    <tbody>
+                    <tr>
                     <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
                     <td>Germany</td>
-                </tr>
-                <tr>
-                    <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
-                    <td>Germany</td>
-                </tr>
-                <tr>
-                    <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
-                    <td>Germany</td>
-                </tr>
-                <tr>
-                    <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
-                    <td>Germany</td>
-                </tr>
-                <tr>
-                    <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
-                    <td>Germany</td>
-                </tr>
-                <tr>
-                    <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
-                    <td>Germany</td>
-                </tr>
-                <tr>
-                    <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
-                    <td>Germany</td>
-                </tr>
-                <tr>
-                    <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
-                    <td>Germany</td>
-                </tr>
-                <tr>
-                    <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
-                    <td>Germany</td>
-                </tr>
-                <tr>
-                    <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
-                    <td>Germany</td>
-                </tr>
-                <tr>
-                    <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
-                    <td>Germany</td>
-                </tr>
-                <tr>
-                    <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
-                    <td>Germany</td>
-                </tr>
-                <tr>
-                    <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
-                    <td>Germany</td>
-                </tr>
-                <tr>
-                    <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
-                    <td>Germany</td>
-                </tr>
+                    </tr>
+                    <tr>
+                        <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
+                        <td>Germany</td>
+                    </tr>
+                    <tr>
+                        <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
+                        <td>Germany</td>
+                    </tr>
+                    <tr>
+                        <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
+                        <td>Germany</td>
+                    </tr>
+                    <tr>
+                        <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
+                        <td>Germany</td>
+                    </tr>
+                    <tr>
+                        <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
+                        <td>Germany</td>
+                    </tr>
+                    <tr>
+                        <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
+                        <td>Germany</td>
+                    </tr>
+                    <tr>
+                        <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
+                        <td>Germany</td>
+                    </tr>
+                    <tr>
+                        <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
+                        <td>Germany</td>
+                    </tr>
+                    <tr>
+                        <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
+                        <td>Germany</td>
+                    </tr>
+                    <tr>
+                        <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
+                        <td>Germany</td>
+                    </tr>
+                    <tr>
+                        <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
+                        <td>Germany</td>
+                    </tr>
+                    <tr>
+                        <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
+                        <td>Germany</td>
+                    </tr>
+                    <tr>
+                        <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
+                        <td>Germany</td>
+                    </tr>
+                    </tbody>
                 </table>
-
             </div>
         </div>
     )
