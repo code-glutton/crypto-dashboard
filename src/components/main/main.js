71c8cac1cedca0a35e110abea9fb4a169c23b9b2
Jonathan-Odia Osahon Emmanuel <emmyodia@gmail.com>
2020-10-26 14:23:23 +0100

    first chart added to ui

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import './main.css'
+import './main.css';
+import { VictoryChart,VictoryLine } from 'victory';
 
 function Main(){
     return(
@@ -37,12 +38,97 @@ function Main(){
                             </div>
                         </div>    
                     </div>
+                    <div className='chart'>
+                    <VictoryChart minDomain={{ y: 0 }} padding={10} width={900}>
+                        <VictoryLine
+                            style={{
+                                data: { stroke: "#c43a31" },
+                                parent: { border: "1px solid #ccc"}
+                                 }}
+                            data={[
+                            { x: 1, y: 2 },
+                            { x: 2, y: 3 },
+                            { x: 3, y: 5 },
+                            { x: 4, y: 4 },
+                            { x: 5, y: 7 }
+                                ]}
+                        />
+                    </VictoryChart>
+                    </div>
                 </div>
                 <div className='mainHistogram mainDisplayItems two'>
                     2
                 </div>
                 <div className='mainTable mainDisplayItems three'>
-                    3
+                <div>
+                    <table className="customers">
+                        <thead>
+                            <tr>
+                                <th colSpan='2'>
+                                    Breaking Crypto News
+                                </th>
+                            </tr> 
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
+                                <td>Germany</td>
+                            </tr>
+                            <tr>
+                                <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
+                                <td>Germany</td>
+                            </tr>
+                            <tr>
+                                <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
+                                <td>Germany</td>
+                            </tr>
+                            <tr>
+                                <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
+                                <td>Germany</td>
+                            </tr>
+                            <tr>
+                                <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
+                                <td>Germany</td>
+                            </tr>
+                            <tr>
+                                <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
+                                <td>Germany</td>
+                            </tr>
+                            <tr>
+                                <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
+                                <td>Germany</td>
+                            </tr>
+                            <tr>
+                                <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
+                                <td>Germany</td>
+                            </tr>
+                            <tr>
+                                <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
+                                <td>Germany</td>
+                            </tr>
+                            <tr>
+                                <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
+                                <td>Germany</td>
+                            </tr>
+                            <tr>
+                                <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
+                                <td>Germany</td>
+                            </tr>
+                            <tr>
+                                <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
+                                <td>Germany</td>
+                            </tr>
+                            <tr>
+                                <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
+                                <td>Germany</td>
+                            </tr>
+                            <tr>
+                                <td width='90%'><h2>Title</h2><p>Alfreds Futterkiste</p></td>
+                                <td>Germany</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
                 </div>
                 <div className='mainFacts mainDisplayItems four'>
                     4
